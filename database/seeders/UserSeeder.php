709a0b1d25c0cffb269bf22d27589cffeb5de64e
[pemenkaret] <pandupes2019@gmail.com>
2024-12-07 14:21:17 +0700

    seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,22 +20,21 @@ class UserSeeder extends Seeder
                 'name' => 'Admin User',
                 'email' => 'admin@example.com',
                 'password' => Hash::make('password'), // Pastikan mengganti password dengan lebih aman
-                'role' => 1, // Sesuaikan dengan ID role di tabel roles
-                'organizer' => 1, // Sesuaikan dengan ID organizer di tabel organizers
-                'refresh_token' => null,
+                'role_id' => 1, // Sesuaikan dengan ID role di tabel roles
+                'organizer_id' => 1, // Sesuaikan dengan ID organizer di tabel organizers
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [
-                'name' => 'Organizer User',
-                'email' => 'organizer@example.com',
-                'password' => Hash::make('password'),
-                'role' => 2,
-                'organizer' => 1,
-                'refresh_token' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+            // [
+            //     'name' => 'Organizer User',
+            //     'email' => 'organizer@example.com',
+            //     'password' => Hash::make('password'),
+            //     'role' => 2,
+            //     'organizer' => 1,
+            //     'refresh_token' => null,
+            //     'created_at' => now(),
+            //     'updated_at' => now(),
+            // ],
         ]);
     }
 }
